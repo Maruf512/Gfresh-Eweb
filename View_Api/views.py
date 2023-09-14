cdb015991bfb53e4f947799cd075774880f8d206
Maruf512 <marufsarkar512@gmail.com>
2023-09-14 22:03:31 +0600

    setup compleate

--- View_Api/views.py
+++ View_Api/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request, 'View_Api/dist/index.html')
+    return render(request, 'index.html')
 
 
 
