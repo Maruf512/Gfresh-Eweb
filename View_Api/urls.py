7f38c2540842e961ce213b5813e7e9251030b914
Maruf512 <marufsarkar512@gmail.com>
2023-09-15 12:35:59 +0600

    cleaning templets

--- View_Api/urls.py
+++ View_Api/urls.py
@@ -5,4 +5,11 @@ urlpatterns = [
     path("", views.home, name="Home"),
     path("shop", views.shop, name="Shop"),
     path("view", views.view, name="View"),
+    path("wishlist", views.wishlist, name="Wishlist"),
+    path("login", views.login, name="Login"),
+    path("register", views.register, name="Register"),
+    path("account", views.account, name="Account"),
+    path("profile-info", views.profile_info, name="Profile-Info"),
+    path("manage-address", views.manage_address, name="Manage-Address"),
+    path("change-password", views.change_password, name="Change-Password"),
 ]
--- View_Api/views.py
+++ View_Api/views.py
@@ -7,10 +7,37 @@ def home(request):
     return render(request, 'index.html')
 
 
-
 def shop(request):
     return render(request, 'shop.html')
+
 
 def view(request):
     return render(request, 'view.html')
 
+
+def wishlist(request):
+    return render(request, 'wishlist.html')
+
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def register(request):
+    return render(request, 'register.html')
+
+
+def account(request):
+    return render(request, 'account.html')
+
+
+def profile_info(request):
+    return render(request, 'profile-info.html')
+
+
+def manage_address(request):
+    return render(request, 'manage-address.html')
+
+
+def change_password(request):
+    return render(request, 'change-password.html')
