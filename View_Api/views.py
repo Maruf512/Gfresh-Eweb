c6ff52cfd7965039491e3de223b311ff38e9510c
Maruf512 <marufsarkar512@gmail.com>
2023-09-19 11:14:42 +0600

    done

--- View_Api/views.py
+++ View_Api/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request, 'index.html')
+    return render(request, 'home.html')
 
 
 def shop(request):
