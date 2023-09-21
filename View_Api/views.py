5b0e7599b5fe1bbc22a2133766f6222622eccce8
Maruf512 <marufsarkar512@gmail.com>
2023-09-21 08:07:49 +0600

    added lebel

--- View_Api/views.py
+++ View_Api/views.py
@@ -3,9 +3,10 @@ from django.contrib.auth.hashers import make_password, check_password   # to mak
 from .models import User
 
 
-# Create your views here.
-
-def get_data(get_data_by):
+# =======================================================
+# ========== Additional Methods =========================
+# =======================================================
+def get_user_data(get_data_by):
     data = []
     all_users = User.objects.all()        #read data from the model
     
@@ -21,15 +22,24 @@ def get_data(get_data_by):
     return data
 
 
+# =======================================================
+# ========== Home Section ===============================
+# =======================================================
 def home(request):
     return render(request, 'home.html')
 
 
+# =======================================================
+# ========== Shop Section ===============================
+# =======================================================
 def shop(request):
     print("Shop")
     return render(request, 'shop.html')
 
 
+# =======================================================
+# ========== View Section ===============================
+# =======================================================
 def view(request):
     return render(request, 'view.html')
 
@@ -38,6 +48,9 @@ def wishlist(request):
     return render(request, 'wishlist.html')
 
 
+# =======================================================
+# ========== Login Section ==============================
+# =======================================================
 def login(request):
     if request.method == "POST":
         login_data = request.POST.dict()
@@ -46,7 +59,7 @@ def login(request):
         if emailAddress != "" and password != "":
             checkpt = User.objects.filter(email=emailAddress)       # check if user email exists or not
             if checkpt:
-                data = get_data(emailAddress)
+                data = get_user_data(emailAddress)
                 check_passwd = check_password(password, data[2])
                 if check_passwd:
                     print("login successfull")
@@ -65,21 +78,21 @@ def login(request):
         return render(request, 'login.html')
 
 
+# =======================================================
+# ========== Register Section ===========================
+# =======================================================
 def register(request):
-    # print(make_password("1212"))
-
     if request.method == "POST":
+        # get all data from form
         register_data = request.POST.dict()
         full_name = register_data.get("first_name")
         email_address = register_data.get("email")
         passward = register_data.get("Password")
         check_passwd = register_data.get("ck_password")
         check_terms = register_data.get("check_terms")
-
         # handel logic system
         if full_name != "" and email_address != "" and passward != "" and check_passwd != "":
             checkpt = User.objects.filter(email=email_address)       # check if user email exists or not
-            print(type(checkpt))
             if checkpt != '':
                 # check if the password matches with the confirm password field
                 if passward == check_passwd:
@@ -90,6 +103,7 @@ def register(request):
                         # push all data to database
                         user = User(first_name=full_name, email=email_address, passwd=Hash_passward)
                         user.save()
+                        return render(request, 'login.html')
                     else:
                         print("Do you agry to our terms and conditions?")
                 else:
@@ -103,33 +117,55 @@ def register(request):
             print("Empty Fields Detected!")
 
         return render(request, 'register.html')
-
+    
     return render(request, 'register.html')
 
 
+
+# =======================================================
+# ========== Account Section ============================
+# =======================================================
 def account(request):
     return render(request, 'account.html')
 
 
+# =======================================================
+# ========== Profile-Info Section =======================
+# =======================================================
 def profile_info(request):
     return render(request, 'profile-info.html')
 
 
+# =======================================================
+# ========== Manage-Address Section =====================
+# =======================================================
 def manage_address(request):
     return render(request, 'manage-address.html')
 
 
+# =======================================================
+# ========== Change-password Section ====================
+# =======================================================
 def change_password(request):
     return render(request, 'change-password.html')
 
 
+# =======================================================
+# ========== Order-complete Section =====================
+# =======================================================
 def order_complete(request):
     return render(request, 'order-complete.html')
 
 
+# =======================================================
+# ========== Cart Section ===============================
+# =======================================================
 def cart(request):
     return render(request, 'cart.html')
 
 
+# =======================================================
+# ========== Checkout Section ===========================
+# =======================================================
 def checkout(request):
     return render(request, 'checkout.html')
