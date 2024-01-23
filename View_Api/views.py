84bc0ca6af6f51e1a29e105151358e4ac00f4813
Maruf512 <marufsarkar512@gmail.com>
2024-01-23 21:25:56 +0600

    login section

--- View_Api/views.py
+++ View_Api/views.py
@@ -1,12 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.hashers import make_password, check_password   # to make a hash password and to read that password
-from .models import User
-
-
-# =======================================================
-# ========== Additional Veriabl =========================
-# =======================================================
-logged_in_user = "Login/Register"
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
 
 # =======================================================
@@ -32,6 +27,12 @@ def get_user_data(get_data_by):
 # ========== Home Section ===============================
 # =======================================================
 def home(request):
+    if request.user.is_authenticated:
+        print("Authenticated user")
+        logged_in_user = request.user
+    else:
+        logged_in_user = "Login/Register"
+        print("non authenticated")
     return render(request, 'home.html', {'user_data':logged_in_user})
 
 
@@ -39,47 +40,88 @@ def home(request):
 # ========== Shop Section ===============================
 # =======================================================
 def shop(request):
+    if request.user.is_authenticated:
+        logged_in_user = request.user
+        print("Authenticated user")
+    else:
+        logged_in_user = "Login/Register"
+        print("non authenticated")
     print("Shop")
-    return render(request, 'shop.html')
+    return render(request, 'shop.html', {'user_data':logged_in_user})
 
 
 # =======================================================
 # ========== View Section ===============================
 # =======================================================
 def view(request):
-    return render(request, 'view.html')
+    if request.user.is_authenticated:
+        logged_in_user = request.user
+        print("Authenticated user")
+    else:
+        logged_in_user = "Login/Register"
+        print("non authenticated")
+    print("Shop")
+    return render(request, 'view.html', {'user_data':logged_in_user})
 
 
 def wishlist(request):
-    return render(request, 'wishlist.html')
+    if request.user.is_authenticated:
+        logged_in_user = request.user
+        print("Authenticated user")
+    else:
+        logged_in_user = "Login/Register"
+        print("non authenticated")
+    print("Shop")
+    return render(request, 'wishlist.html', {'user_data':logged_in_user})
 
 
 # =======================================================
 # ========== Login Section ==============================
 # =======================================================
-def login(request):
+def Login(request):
     if request.method == "POST":
-        login_data = request.POST.dict()
+        login_data = request.POST.dict()                            # [ GET ALL THE INPUT FIELDS DATA ]
+        username = login_data.get("username")
         emailAddress = login_data.get("email")
         password = login_data.get("passwd")
-        if emailAddress != "" and password != "":
-            checkpt = User.objects.filter(email=emailAddress)       # check if user email exists or not
-            if checkpt:
-                data = get_user_data(emailAddress)
-                check_passwd = check_password(password, data[2])
-                if check_passwd:
-                    print("login successfull")
-                    # change page
-                    # to home page
-                    return render(request, 'home.html')
-            else:
-                print("User dosent exist on database!")
-                print("please register.")
-                # send to register page
-                return render(request, 'register.html')
+
+        user = authenticate(request, username=username, email=emailAddress, password=password)
+
+        if user is not None:
+            print("redirect to a success page")
+            login(request, user)
+            return account(request)
+        
         else:
-            print("Empty Field detected!")
-        return render(request, 'login.html')
+            print("Login failed")
+            return render(request, 'login.html')
+
+
+
+
+
+
+        # if emailAddress != "" and password != "":
+        #     checkpt = User.objects.filter(email=emailAddress)       # check if user email exists or not
+        #     if checkpt:
+        #         data = get_user_data(emailAddress)
+        #         check_passwd = check_password(password, data[2])
+        #         user = authenticate(request)
+        #         if check_passwd:
+        #             print("login successfull")
+        #             # change page
+        #             # to home page
+        #             return render(request, 'home.html')
+        #         else:
+        #             print("Wrong Password")
+        #     else:
+        #         print("User dosent exist on database!")
+        #         print("please register.")
+        #         # send to register page
+        #         return render(request, 'register.html')
+        # else:
+        #     print("Empty Field detected!")
+        # return render(request, 'login.html')
     else:
         return render(request, 'login.html')
 
@@ -96,31 +138,37 @@ def register(request):
         passward = register_data.get("Password")
         check_passwd = register_data.get("ck_password")
         check_terms = register_data.get("check_terms")
+
+
+        user = User.objects.create_user(username=full_name, email=email_address, password=passward)
+        user.save()
+
+
         # handel logic system
-        if full_name != "" and email_address != "" and passward != "" and check_passwd != "":
-            checkpt = User.objects.filter(email=email_address)       # check if user email exists or not
-            if checkpt != '':
-                # check if the password matches with the confirm password field
-                if passward == check_passwd:
-                    # check terms and condition's
-                    # generate hash password
-                    Hash_passward = make_password(passward)
-                    if check_terms == "on":
-                        # push all data to database
-                        user = User(first_name=full_name, email=email_address, passwd=Hash_passward)
-                        user.save()
-                        return render(request, 'login.html')
-                    else:
-                        print("Do you agry to our terms and conditions?")
-                else:
-                    print("password didnt match!")
-            else:
-                print("This email already exists!")
-                print("Try to login!")
-                print("reset password if needed")
-                return render(request, 'login.html')
-        else:
-            print("Empty Fields Detected!")
+        # if full_name != "" and email_address != "" and passward != "" and check_passwd != "":
+        #     checkpt = User.objects.filter(email=email_address)       # check if user email exists or not
+        #     if checkpt != '':
+        #         # check if the password matches with the confirm password field
+        #         if passward == check_passwd:
+        #             # check terms and condition's
+        #             # generate hash password
+        #             Hash_passward = make_password(passward)
+        #             if check_terms == "on":
+        #                 # push all data to database
+        #                 user = User.objects.create_user(username=full_name, email=email_address, password=Hash_passward)
+        #                 user.save()
+        #                 return render(request, 'login.html')
+        #             else:
+        #                 print("Do you agry to our terms and conditions?")
+        #         else:
+        #             print("password didnt match!")
+        #     else:
+        #         print("This email already exists!")
+        #         print("Try to login!")
+        #         print("reset password if needed")
+        #         return render(request, 'login.html')
+        # else:
+        #     print("Empty Fields Detected!")
 
         return render(request, 'register.html')
     
@@ -132,6 +180,10 @@ def register(request):
 # ========== Account Section ============================
 # =======================================================
 def account(request):
+    if request.user.is_authenticated:
+        print("Authenticated user")
+    else:
+        print("non authenticated")
     return render(request, 'account.html')
 
 
@@ -139,6 +191,10 @@ def account(request):
 # ========== Profile-Info Section =======================
 # =======================================================
 def profile_info(request):
+    if request.user.is_authenticated:
+        print("Authenticated user")
+    else:
+        print("non authenticated")
     return render(request, 'profile-info.html')
 
 
@@ -146,6 +202,10 @@ def profile_info(request):
 # ========== Manage-Address Section =====================
 # =======================================================
 def manage_address(request):
+    if request.user.is_authenticated:
+        print("Authenticated user")
+    else:
+        print("non authenticated")
     return render(request, 'manage-address.html')
 
 
@@ -153,6 +213,10 @@ def manage_address(request):
 # ========== Change-password Section ====================
 # =======================================================
 def change_password(request):
+    if request.user.is_authenticated:
+        print("Authenticated user")
+    else:
+        print("non authenticated")
     return render(request, 'change-password.html')
 
 
@@ -160,6 +224,10 @@ def change_password(request):
 # ========== Order-complete Section =====================
 # =======================================================
 def order_complete(request):
+    if request.user.is_authenticated:
+        print("Authenticated user")
+    else:
+        print("non authenticated")
     return render(request, 'order-complete.html')
 
 
@@ -167,6 +235,10 @@ def order_complete(request):
 # ========== Cart Section ===============================
 # =======================================================
 def cart(request):
+    if request.user.is_authenticated:
+        print("Authenticated user")
+    else:
+        print("non authenticated")
     return render(request, 'cart.html')
 
 
@@ -174,16 +246,34 @@ def cart(request):
 # ========== Checkout Section ===========================
 # =======================================================
 def checkout(request):
+    if request.user.is_authenticated:
+        print("Authenticated user")
+    else:
+        print("non authenticated")
     return render(request, 'checkout.html')
 
 
+# =======================================================
+# ========== Logout Section ===============================
+# =======================================================
+def Logout(request):
+    logout(request)
+    if request.user.is_authenticated:
+        logged_in_user = request.user
+        print("Authenticated user")
+    else:
+        logged_in_user = "Login/Register"
+        print("non authenticated")
+    return render(request, 'home.html', {'user_data':logged_in_user})
 
 
 # =======================================================
 # ========== Test Section ===============================
 # =======================================================
 def test(request):
+    if request.user.is_authenticated:
+        print("Authenticated user")
+    else:
+        print("non authenticated")
     return render(request, 'test.html')
-
-
 
